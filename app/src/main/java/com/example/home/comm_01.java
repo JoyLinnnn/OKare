71a8dd6358c930cdb276c2b4381032ad8b2d41d9
JoyLinnnn <armyjoyhappy0526@gmail.com>
2021-06-25 23:36:59 +0800

    新增飲食推薦細項頁面

--- app/src/main/java/com/example/home/comm_01.java
+++ app/src/main/java/com/example/home/comm_01.java
@@ -2,7 +2,10 @@ package com.example.home;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class comm_01 extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class comm_01 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comm_01);
+
+        ImageButton 通_通知_IB=findViewById(R.id.通_通知_IB);
+        通_通知_IB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(comm_01.this,noti.class);
+                startActivity(intent);
+            }
+        });
     }
 }
