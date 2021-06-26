589610c98b6b8cae9ed3a02fcdcfa75bef19c7e9
shannnnnn <tt265206@gmail.com>
2021-06-27 00:24:29 +0800

    飲食推薦文字(除了癌症)程式碼修改

--- app/src/main/java/com/example/home/comm_02.java
+++ app/src/main/java/com/example/home/comm_02.java
@@ -4,8 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
-import android.view.View;
-import android.widget.ImageButton;
 import android.widget.TextView;
 
 import java.io.BufferedReader;
