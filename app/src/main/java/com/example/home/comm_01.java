74f78bd6814a67136b67899eeb0e0eb05bc38eea
shannnnnn <tt265206@gmail.com>
2021-06-27 00:16:29 +0800

    飲食推薦文字(除了癌症)全部上傳&字體調大

--- app/src/main/java/com/example/home/comm_01.java
+++ app/src/main/java/com/example/home/comm_01.java
@@ -7,8 +7,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 
-public class comm_01 extends AppCompatActivity {
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class comm_01 extends AppCompatActivity {
+    TextView 飲_糖尿病內文_TV;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,5 +29,25 @@ public class comm_01 extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        飲_糖尿病內文_TV = findViewById(R.id.飲_糖尿病內文_TV);
+        // 資料若超過頁面，須設定可以Scrolling
+        飲_糖尿病內文_TV.setMovementMethod(ScrollingMovementMethod.getInstance());
+
+        // 讀取 raw folder 的檔案
+        InputStreamReader isr = new InputStreamReader(this.getResources().openRawResource(R.raw.food_candy));
+        BufferedReader br = new BufferedReader(isr);
+        StringBuilder sb = new StringBuilder();
+        String line;
+        try {
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+                sb.append("\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        飲_糖尿病內文_TV.setText(sb.toString());
     }
 }
--- app/src/main/java/com/example/home/comm_06.java
+++ app/src/main/java/com/example/home/comm_06.java
@@ -3,12 +3,41 @@ package com.example.home;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class comm_06 extends AppCompatActivity {
-
+    TextView 飲_脂肪肝內文_TV;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comm_06);
+
+
+        飲_脂肪肝內文_TV = findViewById(R.id.飲_脂肪肝內文_TV);
+        // 資料若超過頁面，須設定可以Scrolling
+        飲_脂肪肝內文_TV.setMovementMethod(ScrollingMovementMethod.getInstance());
+
+        // 讀取 raw folder 的檔案
+        InputStreamReader isr = new InputStreamReader(this.getResources().openRawResource(R.raw.food_heart));
+        BufferedReader br = new BufferedReader(isr);
+        StringBuilder sb = new StringBuilder();
+        String line;
+        try {
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+                sb.append("\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        飲_脂肪肝內文_TV.setText(sb.toString());
     }
 }
