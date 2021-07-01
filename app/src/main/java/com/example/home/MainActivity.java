31ffd704f518f5162653f44ff88931bbcaffc4ca
USER <steven1990620@gmail.com>
2021-07-02 00:28:43 +0800

    室內影像程式碼修改

--- app/src/main/java/com/example/home/MainActivity.java
+++ app/src/main/java/com/example/home/MainActivity.java
@@ -24,7 +24,6 @@ import static com.example.home.R.id.居_衛教資訊_IB;
 public class MainActivity extends AppCompatActivity {
     DatabaseReference mydb;
     TextView temp,hum;
- /*   WebView web;*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,12 +31,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         temp=(TextView)findViewById(R.id.temp);
         hum=(TextView)findViewById(R.id.hum);
-/*        web=findViewById(R.id.webView);*/
         mydb= FirebaseDatabase.getInstance().getReference().child("DHT11SensorData");
-/*        WebSettings webSettings=web.getSettings();
-        ((WebSettings) webSettings).setJavaScriptEnabled(true);
-        web.setWebViewClient(new Callback());
-        web.loadUrl("http://192.168.35.52/");*/
         try {
 
             mydb.addValueEventListener(new ValueEventListener() {
@@ -116,11 +110,6 @@ public class MainActivity extends AppCompatActivity {
 
 
         }
-    private class Callback extends WebViewClient {
-        @Override
-        public boolean shouldOverrideKeyEvent(WebView view, KeyEvent event) {
-            return false;
-        }
-    }
+
 
     }
--- app/src/main/java/com/example/home/Webview.java
+++ app/src/main/java/com/example/home/Webview.java
@@ -3,12 +3,27 @@ package com.example.home;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class Webview extends AppCompatActivity {
-
+    WebView web;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_webview);
+        web=findViewById(R.id.webView);
+        WebSettings webSettings=web.getSettings();
+        ((WebSettings) webSettings).setJavaScriptEnabled(true);
+        web.setWebViewClient(new Callback());
+        web.loadUrl("http://192.168.35.52/");
+    }
+    private class Callback extends WebViewClient {
+        @Override
+        public boolean shouldOverrideKeyEvent(WebView view, KeyEvent event) {
+            return false;
+        }
     }
 }
