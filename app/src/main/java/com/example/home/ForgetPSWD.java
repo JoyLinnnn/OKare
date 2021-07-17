080b9ef67495be34a163a1d5b7d87789529061ae
Evelyn <407630028@gms.tku.edu.tw>
2021-07-17 14:26:24 +0800

    新增忘記密碼介面(forgetpswd)

---
+++ app/src/main/java/com/example/home/ForgetPSWD.java
@@ -0,0 +1,14 @@
+package com.example.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ForgetPSWD extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forget_p_s_w_d);
+    }
+}
